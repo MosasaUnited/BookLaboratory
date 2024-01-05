3172d6ed916076b5c7aef5d7c44bf714d8f34fe8
MosasaUnited <mostafa447@hotmail.com>
2024-01-05 21:57:16 +0200

    CreatingSplashViewBody

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'feature/prsentation/views/splash_view.dart';
+
 void main() {
   runApp(const BookLab());
 }
