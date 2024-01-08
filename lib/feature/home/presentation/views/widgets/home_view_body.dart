2d5bbfb97c1a128dfeaaf38ea02033c44a1c5e88
MosasaUnited <mostafa447@hotmail.com>
2024-01-08 21:29:40 +0200

    AddFeaturedListViewItemToHome

--- lib/feature/home/presentation/views/widgets/home_view_body.dart
+++ lib/feature/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'featured_books_list_view.dart';
+import 'featured_list_view_items.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
