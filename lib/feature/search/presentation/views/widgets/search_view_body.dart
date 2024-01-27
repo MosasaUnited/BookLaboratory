b64970b61716190900092a720e7c7247d8358031
MosasaUnited <mostafa447@hotmail.com>
2024-01-27 21:46:05 +0200

    SearchUiDone

--- lib/feature/search/presentation/views/widgets/search_view_body.dart
+++ lib/feature/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/feature/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/styles.dart';
 import 'custom_search_textfield.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -10,6 +12,14 @@ class SearchViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomSearchTextField(),
+        SizedBox(
+          height: 16,
+        ),
+        Text('Best Seller', style: Styles.textStyle18),
+        SizedBox(
+          height: 16,
+        ),
+        Expanded(child: SearchResultListView()),
       ],
     );
   }
