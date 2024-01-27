d9eb185ea18271a0927afbfa2d4419a941fc4690
MosasaUnited <mostafa447@hotmail.com>
2024-01-27 13:48:29 +0200

    RefactorDetailsBooksView

--- lib/feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,7 @@
-import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/feature/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly/feature/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly/feature/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
-import 'book_rating.dart';
-import 'books_action.dart';
+import 'books_details_section.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -12,66 +9,22 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 33.5),
+            padding: EdgeInsets.symmetric(horizontal: 33.5),
             child: Column(
               children: [
-                const CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * .17),
-                  child: const CustomBookImage(),
-                ),
-                const SizedBox(
-                  height: 18.5,
-                ),
-                const Text(
-                  'Atomic Habits',
-                  style: Styles.textStyle30,
-                ),
-                Opacity(
-                  opacity: .7,
-                  child: Text(
-                    'James Clear',
-                    style: Styles.textStyle18.copyWith(
-                      fontWeight: FontWeight.w900,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 23.5,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    BookRating(),
-                  ],
-                ),
-                const SizedBox(
-                  height: 37,
-                ),
-                const BooksAction(),
-                const Expanded(
+                CustomBookDetailsAppBar(),
+                BooksDetailsSection(),
+                Expanded(
                   child: SizedBox(
                     height: 50,
                   ),
                 ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text('You can also like',
-                      style: Styles.textStyle18.copyWith(
-                        fontWeight: FontWeight.w600,
-                      )),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                const SimilarBooksListView(),
+                SimilarBooksSection(),
               ],
             ),
           ),
---
+++ lib/feature/home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+import 'book_rating.dart';
+import 'books_action.dart';
+import 'custom_book_item.dart';
+
+class BooksDetailsSection extends StatelessWidget {
+  const BooksDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .17),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 18.5,
+        ),
+        const Text(
+          'Atomic Habits',
+          style: Styles.textStyle30,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'James Clear',
+            style: Styles.textStyle18.copyWith(
+              fontWeight: FontWeight.w900,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 23.5,
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BookRating(),
+          ],
+        ),
+        const SizedBox(
+          height: 37,
+        ),
+        const BooksAction(),
+      ],
+    );
+  }
+}
