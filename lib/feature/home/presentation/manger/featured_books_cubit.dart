01f1de003a5a44d62a2c2eeb6e4d2978682a890f
MosasaUnited <mostafa447@hotmail.com>
2024-02-10 21:01:00 +0200

    CreateFeaturedBooksMethod

--- lib/feature/home/presentation/manger/featured_books_cubit.dart
+++ lib/feature/home/presentation/manger/featured_books_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/feature/home/data/repository/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,5 +7,17 @@ import '../../data/models/books_model.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFetauredBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
