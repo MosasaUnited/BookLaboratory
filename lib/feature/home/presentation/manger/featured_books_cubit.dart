15cb1e410f33165a5692581e3dae3a3d7f02a7b8
MosasaUnited <mostafa447@hotmail.com>
2024-02-10 20:49:39 +0200

    CreateFeaturedBooksCubitState

--- lib/feature/home/presentation/manger/featured_books_cubit.dart
+++ lib/feature/home/presentation/manger/featured_books_cubit.dart
@@ -1,6 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:equatable/equatableta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../data/models/books_model.dart';
 
