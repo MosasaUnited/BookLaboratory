d0d6fd18e49cf88161f86e02e191d27d4d997631
MosasaUnited <mostafa447@hotmail.com>
2024-02-10 20:48:08 +0200

    CreateFeaturedBooksCubitState

--- lib/feature/home/presentation/manger/featured_books_cubit.dart
+++ lib/feature/home/presentation/manger/featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:equatable/equatableta.dart';
 
 import '../../data/models/books_model.dart';
--- lib/feature/home/presentation/manger/featured_books_state.dart
+++ lib/feature/home/presentation/manger/featured_books_state.dart
@@ -3,7 +3,6 @@ part of 'featured_books_cubit.dart';
 abstract class FeaturedBooksState extends Equatable {
   const FeaturedBooksState();
 
-  @override
   List<Object> get props => [];
 }
 
