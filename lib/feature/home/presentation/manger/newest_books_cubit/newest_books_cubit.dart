22f10864c7025a58c6dd681f2a3641659b90bfee
MosasaUnited <mostafa447@hotmail.com>
2024-02-10 21:13:30 +0200

    CreateNewestBooksMethod

--- lib/feature/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
+++ lib/feature/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -1,11 +1,23 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../data/models/books_model.dart';
+import '../../../data/repository/home_repo.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
 }
