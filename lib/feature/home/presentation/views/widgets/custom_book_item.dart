ea34fd8d796adf0f06168095ee39a1a2678efd00
MosasaUnited <mostafa447@hotmail.com>
2024-03-01 15:14:35 +0200

    CachedNetworkImage

--- lib/feature/home/presentation/views/widgets/custom_book_item.dart
+++ lib/feature/home/presentation/views/widgets/custom_book_item.dart
@@ -9,12 +9,22 @@ class CustomBookImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.7 / 4,
+      aspectRatio: 2.8 / 4,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(12),
         child: CachedNetworkImage(
           fit: BoxFit.fill,
           imageUrl: image,
+          placeholder: (context, url) => const Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 8.0,
+            ),
+          ),
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error_outline_outlined,
+            size: 50,
+            color: Colors.redAccent,
+          ),
         ),
       ),
     );
