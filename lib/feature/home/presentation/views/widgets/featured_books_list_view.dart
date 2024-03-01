2ef858ec1f5951f2a1da53a9e2cfb1f1298eb63e
MosasaUnited <mostafa447@hotmail.com>
2024-03-01 14:40:13 +0200

    DisplayFeaturedBooksListView2

--- lib/feature/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/feature/home/presentation/views/widgets/featured_books_list_view.dart
@@ -17,6 +17,8 @@ class FeaturedBooksListView extends StatelessWidget {
           return SizedBox(
             height: MediaQuery.of(context).size.height * .3,
             child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemCount: state.books.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
                   return Padding(
